742e7ac758fa5645b7136180dbefe873ddaa85f5
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-09 11:04:58 +0600

    object partic

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,2 +1,16 @@
 let a  = `Bangladesh's`;
 console.log(a);
+
+let x = 5;
+
+const myObject = {
+    myName: "arif",
+    myAge: 26,
+    myAddress: "Dhaka,\
+    Bangladesh",
+};
+
+myObject.myAddress = "Pabna";
+myObject.myOccupation = "Software Engineer";
+
+console.log(myObject);
