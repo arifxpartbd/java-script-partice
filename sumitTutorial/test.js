696b25048664d935f572fc1ae8dd3b332a344800
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-09 12:07:08 +0600

    for each and show all itemm

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -4,15 +4,18 @@ let carListLenght = carList.length;
 
 let text = "<ul>"
 
-for(let i = 0; i < carListLenght; i ++){
-    text += "<li>" + carList[i] + "</li>";
-    console.log(carList[i]);
+// for(let i = 0; i < carListLenght; i ++){
+//     text += "<li>" + carList[i] + "</li>";
+//     console.log(carList[i]);
+// }
+carList.forEach(myFunction);
+
+function myFunction(value){
+    text += "<li>" + value + "</li>";
 }
 text += "</ul>"
+
 
 document.getElementById("demo").innerHTML = text;
 // carList.forEach(myFunction);
 
-// function myFunction(value){
-//     console.log(value);
-// }
