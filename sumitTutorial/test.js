5abc5368ec88a10a277b3bea230f3a0c079b8ab5
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-11 16:58:29 +0600

    object sorting

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,20 +1,21 @@
 
-const myList = ["arif", "khan", "mina", "bangladesh","culue","rupu", "zoly","ykon"];
-myList.sort();
-myList.reverse();
+const cars = [
+    {type: "Volvo", year: 2016},
+    {type: "Saab", year: 2001},
+    {type: "BMW", year: 2010}
+]
 
-console.log(myList);
+// cars.sort(function(a,b){
+//     return a.year-b.year;
+// });
+cars.sort(mySorting)
 
-const points = [40, 100, 1, 5, 25, 10];
-
-points.sort(function(a, b){
-    return a - b;
-});
-
-function myTopValue(points){
-    return Math.max.apply(null, points);
+function mySorting(a, b){
+    let x = a.type.toLowerCase();
+    let y = b.type.toLowerCase();
+    if(x < y) {return -1;}
+    if(x > y) {return 1;}
+    return 0;
 }
-console.log(points);
-console.log(points[points.length - 1]);
 
-console.log(myTopValue(points));
+console.log(cars);
