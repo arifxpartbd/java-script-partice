3347848962fe80d78dc4a1b11c8232df7084242a
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-11 14:46:55 +0600

    top and low value in list

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -10,5 +10,11 @@ const points = [40, 100, 1, 5, 25, 10];
 points.sort(function(a, b){
     return a - b;
 });
+
+function myTopValue(points){
+    return Math.max.apply(null, points);
+}
 console.log(points);
 console.log(points[points.length - 1]);
+
+console.log(myTopValue(points));
