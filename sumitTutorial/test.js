f89f61adcdce48d2957f9ec0bbe802605f8ad186
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-11 12:52:34 +0600

    how to get top and low value on list

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,7 +1,14 @@
 
-const carList = [];
+const myList = ["arif", "khan", "mina", "bangladesh","culue","rupu", "zoly","ykon"];
+myList.sort();
+myList.reverse();
 
-carList[0] = "arif";
-carList[1] = "khan";
+console.log(myList);
 
-console.log(carList);
+const points = [40, 100, 1, 5, 25, 10];
+
+points.sort(function(a, b){
+    return a - b;
+});
+console.log(points);
+console.log(points[points.length - 1]);
