bad4420b305cae005e115fab2f202fc235d8305b
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-12 20:40:30 +0600

    map print

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,10 +1,13 @@
 const myNumbers = [45, 50, 60];
 
-myNumbers.forEach(myFunction)
 
-function myFunction(value, index, array){
-    console.log(value);
-    console.log(index);
-    console.log(array);
-    console.log("-------------");
+
+function myFunction(value){
+    return value *2 ;
 }
+
+
+const myNewNumber = myNumbers.map(myFunction);
+
+console.log(myNumbers);
+console.log(myNewNumber);
