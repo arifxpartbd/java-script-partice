addf0ac1a88b428a353e9cadd21e6bc0f64e5644
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-12 20:31:01 +0600

    for each

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,21 +1,10 @@
+const myNumbers = [45, 50, 60];
 
-const cars = [
-    {type: "Volvo", year: 2016},
-    {type: "Saab", year: 2001},
-    {type: "BMW", year: 2010}
-]
+myNumbers.forEach(myFunction)
 
-// cars.sort(function(a,b){
-//     return a.year-b.year;
-// });
-cars.sort(mySorting)
-
-function mySorting(a, b){
-    let x = a.type.toLowerCase();
-    let y = b.type.toLowerCase();
-    if(x < y) {return -1;}
-    if(x > y) {return 1;}
-    return 0;
+function myFunction(value, index, array){
+    console.log(value);
+    console.log(index);
+    console.log(array);
+    console.log("-------------");
 }
-
-console.log(cars);
