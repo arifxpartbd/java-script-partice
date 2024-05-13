2e20bfa9a3b29cc4ee7c911312095fe1d3e9ec47
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-14 02:22:47 +0600

    array push method

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,27 +1,6 @@
-const myNumbers = [50, 44, 52, 81, 93, 100, 96, 98, 98];
-
-for(i = 0; i < myNumbers.length; i ++){
-    console.log(myNumbers[i]);
-}
-
-myNumbers.forEach(myForLoop);
-
-function myForLoop(value){
-    console.log(value);
-}
-
-myNumbers.map(myMap);
-
-function myMap(value){
-    console.log(value);
-}
-
-myNumbers.filter(myFilter);
-
-function myFilter(value){
-    return value > 90;
-}
-
-const myNewFilterValue = myNumbers.filter(myFilter);
-
-console.log(myNewFilterValue);
+const myList = ["arif", "asif", "rupu"];
+console.log(myList);
+myList.push("lamia");
+console.log(myList);
+myList[0] = "mahmuda";
+console.log(myList);
