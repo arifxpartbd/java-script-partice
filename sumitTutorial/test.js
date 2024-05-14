b24507c6d4aa60d3f9022e5b330c3f9ba99d0eea
arifxpartbd <arifkhancsebd@gmail.com>
2024-05-14 18:43:06 +0600

    show random number to specific number

--- sumitTutorial/test.js
+++ sumitTutorial/test.js
@@ -1,4 +1,7 @@
-const newNumber = Math.floor(Math.random()* 100) +1;
-
-console.log(newNumber);
+function myFunction(sValue, eValue) {
+    // Generate a random number between eValue and sValue
+    const randomNumber = Math.floor(Math.random() * sValue) + eValue;
+    console.log(randomNumber);
+    return randomNumber;
+}
 
